9b60cd7973d0216d17fa58ecc2bd67148981e9e7
Andres Berejnoi <andresberejnoi@gmail.com>
2021-11-06 16:00:25 -0400

    fixed some typos in funcion

--- tools.py
+++ tools.py
@@ -1,5 +1,8 @@
 
-def parse_RAVDESS_filenames(filename_list):
+from pathlib import Path 
+
+def parse_RAVDESS_filename(filename):
+    '''Parse filenames from RAVDESS audio file'''
     reference_identifiers_dict = {
         'modality': {
             '01':'full-AV',
@@ -23,30 +26,38 @@ def parse_RAVDESS_filenames(filename_list):
             '02':"Dogs are sitting by the door"
         },
         'repetition':{'01':'1st repetition','02':'2nd repetition'},
-        'actor': {i:'male' if i % 2==1 else 'female' for i in range(1,25)}    #I could have made it simpler, but this has an actual key for every value. Odd numbers are labeled 'male' and even values are labeled 'female'
+        'actor': {f"{i:02}":'male' if i % 2==1 else 'female' for i in range(1,25)},    #I could have made it simpler, but this has an actual key for every value. Odd numbers are labeled 'male' and even values are labeled 'female'
+        #'actor':{0:'female', 1:'male'},
     }
 
-    filename_data = {}
+    
+    all_files_dict = {}
 
-    for _name in filename_list:
-        split_identifiers = _name.split('-')
+    #for _name in filename_list:
+    _path = Path(filename)
+    extension = _path.suffix
+    base_name = _path.stem
+    split_identifiers = base_name.split('-')
+    filename_data = {'extension':extension}
 
-        for i, identifier in enumerate(split_identifiers):
-            if i==0:
-                filename_data['modality'] = reference_identifiers_dict['modality'][identifier]
-            elif i==1:
-                filename_data['vocal_channel'] = reference_identifiers_dict['vocal_channel'][identifier]
-            elif i==2:
-                filename_data['emotion'] = reference_identifiers_dict['emotion'][identifier]
-            elif i==3:
-                filename_data['emotional_intensity'] = reference_identifiers_dict['emotional_intensity'][identifier]
-            elif i==4:
-                filename_data['statement'] = reference_identifiers_dict['statement'][identifier]
-            elif i==5:
-                filename_data['repetition'] = reference_identifiers_dict['repetition'][identifier]
-            elif i==6:
-                filename_data['gender'] = reference_identifiers_dict['actor'][identifier]
-                filename_data['actor_id'] = int(identifier)
+    for i, identifier in enumerate(split_identifiers):
+        if i==0:
+            filename_data['modality'] = reference_identifiers_dict['modality'][identifier]
+        elif i==1:
+            filename_data['vocal_channel'] = reference_identifiers_dict['vocal_channel'][identifier]
+        elif i==2:
+            filename_data['emotion'] = reference_identifiers_dict['emotion'][identifier]
+        elif i==3:
+            filename_data['emotional_intensity'] = reference_identifiers_dict['emotional_intensity'][identifier]
+        elif i==4:
+            filename_data['statement'] = reference_identifiers_dict['statement'][identifier]
+        elif i==5:
+            filename_data['repetition'] = reference_identifiers_dict['repetition'][identifier]
+        elif i==6:
+            filename_data['gender'] = reference_identifiers_dict['actor'][identifier]
+            filename_data['actor_id'] = int(identifier)
+
+    #all_files_dict[filename_data['actor_id']] = filename_data
 
     return filename_data
             
