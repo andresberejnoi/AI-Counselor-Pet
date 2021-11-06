89cd9e38c6ccd188ecdcc6b0ceda53924b5ffd05
Andres Berejnoi <andresberejnoi@gmail.com>
2021-11-06 16:42:32 -0400

    Added several to parse different parts of a filename in the RAVDESS audio dataset

--- tools.py
+++ tools.py
@@ -1,36 +1,72 @@
 
 from pathlib import Path 
 
+reference_identifiers_dict = {
+    'modality': {
+        '01':'full-AV',
+        '02':'video-only',
+        '03':'audio-only'
+    },
+    'vocal_channel': {'01':'speech', '02':'song'},
+    'emotion':{
+        '01':'neutral',
+        '02':'calm',
+        '03':'happy',
+        '04':'sad',
+        '05':'angry',
+        '06':'fearful',
+        '07':'disgust',
+        '08':'surprised'
+    },
+    'emotional_intensity':{'01':'normal', '02':'strong'},
+    'statement':{
+        '01':"Kids are talking by the door",
+        '02':"Dogs are sitting by the door"
+    },
+    'repetition':{'01':'1st repetition','02':'2nd repetition'},
+    'actor': {f"{i:02}":'male' if i % 2==1 else 'female' for i in range(1,25)},    #I could have made it simpler, but this has an actual key for every value. Odd numbers are labeled 'male' and even values are labeled 'female'
+    #'actor':{0:'female', 1:'male'},
+}
+
+def get_identifier_by_idx(filename, idx):
+    path = Path(filename)
+    base_name = path.stem 
+
+    str_id = base_name.split('-')[idx]
+    return str_id
+
+def get_extension_from_filename(filename):
+    path = Path(filename)
+    ext  = path.suffix
+    return ext
+
+def get_vocal_channel_from_filename(filename):
+    voc_chan_id = get_identifier_by_idx(filename, 1)
+    return reference_identifiers_dict['vocal_channel'][voc_chan_id]
+
+def get_emotion_from_filename(filename):
+    emo_id = get_identifier_by_idx(filename, 2)
+    return reference_identifiers_dict['emotion'][emo_id]
+
+def get_intensity_from_filename(filename):
+    intensity_id = get_identifier_by_idx(filename, 3)
+    return reference_identifiers_dict['emotional_intensity'][intensity_id]
+
+def get_statement_from_filename(filename):
+    statement_id = get_identifier_by_idx(filename, 4)
+    return reference_identifiers_dict['statement'][statement_id]
+
+def get_repetition_from_filename(filename):
+    rep_id = get_identifier_by_idx(filename, 5)
+    return reference_identifiers_dict['repetition'][rep_id]
+
+def get_actor_id_from_fileame(filename):
+    str_id = get_identifier_by_idx(filename, -1)  #the last item is the actor id
+    return int(str_id)
+
 def parse_RAVDESS_filename(filename):
     '''Parse filenames from RAVDESS audio file'''
-    reference_identifiers_dict = {
-        'modality': {
-            '01':'full-AV',
-            '02':'video-only',
-            '03':'audio-only'
-        },
-        'vocal_channel': {'01':'speech', '02':'song'},
-        'emotion':{
-            '01':'neutral',
-            '02':'calm',
-            '03':'happy',
-            '04':'sad',
-            '05':'angry',
-            '06':'fearful',
-            '07':'disgust',
-            '08':'surprised'
-        },
-        'emotional_intensity':{'01':'normal', '02':'strong'},
-        'statement':{
-            '01':"Kids are talking by the door",
-            '02':"Dogs are sitting by the door"
-        },
-        'repetition':{'01':'1st repetition','02':'2nd repetition'},
-        'actor': {f"{i:02}":'male' if i % 2==1 else 'female' for i in range(1,25)},    #I could have made it simpler, but this has an actual key for every value. Odd numbers are labeled 'male' and even values are labeled 'female'
-        #'actor':{0:'female', 1:'male'},
-    }
 
-    
     all_files_dict = {}
 
     #for _name in filename_list:
