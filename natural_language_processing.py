5ac872c1b691951c64b33dfa4f6c32e8dd0136a6
Andres Berejnoi <andresberejnoi@gmail.com>
2021-11-07 01:04:55 -0400

    Moved code to tools.py and created a class for sentiment analysis with NLTK

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -1,46 +1,28 @@
 '''NLP-related code will go here'''
 import nltk
-from tools import get_identifier_by_idx, parse_RAVDESS_filename
+from nltk.sentiment import SentimentIntensityAnalyzer
+
 import pandas as pd
 import os
 
-from collections import defaultdict
-
-
-def get_all_file_info_dicts(data_root='./datasets'):
-    '''In this lazy route, I will assume that the data is subdivided into 
-    different categories (emotions)'''
-    dict_for_df = defaultdict(list)    #this is a sort of dictionary that will default it's values to lists
-    all_info_dicts = []
-    all_dir_files = os.scandir(data_root)
-    for _dir in all_dir_files:
-        sub_folder = _dir.name
-        #print(f"Current file object `{sub_folder}` is dir? -> {os.path.isdir(sub_folder)}")
-        sub_path = os.path.join(data_root, sub_folder)
-        if os.path.isdir(sub_path):
-            #sub_path = os.path.join(data_root, sub_folder)
-            #print(f"About to enter into sub-folder {sub_path}")
-            for dir_file in os.scandir(sub_path):
-                filename = dir_file.name
-                file_info_dict = parse_RAVDESS_filename(filename)
-                file_info_dict['path'] = os.path.join(sub_path, filename)
-                #print(f".-> adding file {filename} to all_info_dicts")
-                all_info_dicts.append(file_info_dict)
-
-    return all_info_dicts
-
-def create_df_from_dataset(data_root='./datasets', dataset='ravdess'):
-    all_info_dicts = get_all_file_info_dicts(data_root)
-
-    #code for merging dictionaries taken from StackOverflow answer here: https://stackoverflow.com/questions/5946236/how-to-merge-multiple-dicts-with-same-key-or-different-key
-    dict_for_df = defaultdict(list)
-
-    for d in all_info_dicts: # you can list as many input dicts as you want here
-        for key, value in d.items():
-            dict_for_df[key].append(value)
-
-    df = pd.DataFrame(dict_for_df)
-    return df
-
+def guess_emotion(text, analyzer):
+    '''Analyzer is the sentiment analyzer from nltk, at least for now'''
+    result = analyzer.polarity_scores(text)
+    return result
 
                 
+class NLTKEmotionAnalyzer(object):
+    def __init__(self):
+        self.analyzer = SentimentIntensityAnalyzer()
+
+    def guess_emotion(self, text):
+        result = self.analyzer.polarity_scores(text)  #this is a dictionary
+        highest = 0
+        emotion_highest = ''
+        for emotion in result:
+            val = result[emotion]
+            if val > highest:
+                highest = val
+                emotion_highest = emotion 
+        return emotion_highest, highest    #emotion and its likelihood value
+    
